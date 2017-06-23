ce64c48264a69fc02bcc0a77fb0d25a7178feb2e
baptpim <baptiste.pimont@viacesi.fr>
2017-06-23 16:36:15 +0200

    NEW KeyManager

--- BoulderDash/controller/src/main/java/controller/KeyManager.java
+++ BoulderDash/controller/src/main/java/controller/KeyManager.java
@@ -38,4 +38,5 @@ public class KeyManager implements IkeyManager{
 		
 	}
 
+	
 }
--- BoulderDash/view/src/main/java/view/KeyManager.java
+++ BoulderDash/view/src/main/java/view/KeyManager.java
@@ -4,29 +4,41 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyManager extends Panel implements KeyListener, IkeyManager{
-
-	private boolean[] keys;
-	
-	public KeyManager(String title, int width, int height) {
-		super(title, width, height);
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private GameFrame gameFrame;
+	 private KeyManager keyManager;
+	 private String title;
+	    
+	 public KeyManager(GraphicsBuilder graphicsBuilder) {
+		super(graphicsBuilder);
 		// TODO Auto-generated constructor stub
+		keyManager = new KeyManager(null);
+		gameFrame = new GameFrame(title,null, null, null);
+		gameFrame.getFrame().addKeyListener(keyManager);
 	}
+
+
 	@Override
 	public void keyPressed(KeyEvent e) {
-
 		// TODO Auto-generated method stub
-		keys[e.getKeyCode()] = true;
-		System.out.println("pressed!");
+		
 	}
+
 	@Override
 	public void keyReleased(KeyEvent e) {
-		
+		// TODO Auto-generated method stub
 		
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
-	}
-	
+	}	
+	public KeyManager getKeyManager(){
+	    	return keyManager;
+	    }
 }
