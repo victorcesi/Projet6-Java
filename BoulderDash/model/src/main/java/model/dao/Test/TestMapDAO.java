1dc34fc8c1273603c1436c7571a5541f3e2224f4
victorcesi <victor.personnettaz@viacesi.fr>
2017-06-23 15:44:07 +0200

    MapDAO test

---
+++ BoulderDash/model/src/main/java/model/dao/Test/TestMapDAO.java
@@ -0,0 +1,41 @@
+package model.dao.Test;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import model.dao.MapDAO;
+
+public class TestMapDAO {
+	
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void readInDB() throws SQLException {
+		
+		assertNotNull("mapCode is not null", MapDAO.readInDB(1));
+		
+	}
+
+}
